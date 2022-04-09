4e13ecd66520eec5f4274cb2608d1085677c6292
Chun Hui Chang <jill20021@gmail.com>
2022-04-09 22:20:49 +0800

    feat: change rootViewController

--- MyAnimeList/ViewController.swift
+++ MyAnimeList/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var viewModel: String
+    
+    init(viewModel: String) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.setupUI()
     }
-
-
+    
+    private func setupUI() {
+        self.view.backgroundColor = UIColor(displayP3Red: 224.0/255.0, green: 216.0/255.0, blue: 200.0/255.0, alpha: 1)
+    }
 }
 
