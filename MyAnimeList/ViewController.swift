e5b59cd41f26a61ce0b0b8c9e5d61db0ad8e2dff
Chun Hui Chang <jill20021@gmail.com>
2022-04-09 21:27:18 +0800

    Initial Commit

---
+++ MyAnimeList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAnimeList
+//
+//  Created by Jill Chang on 2022/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
