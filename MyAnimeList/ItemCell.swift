6da403d27ff7fc1d527c2eef3a910cfb518be164
Chun Hui Chang <jill20021@gmail.com>
2022-04-11 15:01:35 +0800

    feat: add Anime item layout

---
+++ MyAnimeList/ItemCell.swift
@@ -0,0 +1,169 @@
+//
+//  ItemCell.swift
+//  MyAnimeList
+//
+//  Created by Jill Chang on 2022/4/11.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.textColor = .black
+        label.numberOfLines = 2
+        label.text = "titleLabel"
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.image = UIImage.init(named: "greenbug")
+        return imgView
+    }()
+    
+    
+    private lazy var dateTitles: [UILabel] = {
+        var views = [UILabel]()
+        for text in ["Start date","End date"] {
+            let label = UILabel()
+            label.font = .systemFont(ofSize: 12, weight: .light)
+            label.textColor = .darkGray
+            label.text = text
+            views.append(label)
+        }
+        return views
+    }()
+    
+    private lazy var dateDetails: [UILabel] = {
+        var views = [UILabel]()
+        for text in 0..<2 {
+            let label = UILabel()
+            label.font = .systemFont(ofSize: 20, weight: .regular)
+            label.textColor = .systemGray
+            label.text = "date\(text)"
+            views.append(label)
+        }
+        return views
+    }()
+    
+    private lazy var dateTitleStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: dateTitles)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5.0
+        return stackView
+    }()
+    
+    private lazy var dateDetailStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: dateDetails)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5.0
+        return stackView
+    }()
+    
+    private lazy var rankLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.textColor = .white
+        label.backgroundColor = .blue
+        label.text = "# 14"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var starButton: UIButton = {
+        let button = UIButton()
+        let normalImage = UIImage(systemName: "star")?.withTintColor(.systemYellow, renderingMode: .alwaysOriginal)
+        let selectedImage = UIImage(systemName: "star.fill")?.withTintColor(.systemYellow, renderingMode: .alwaysOriginal)
+        button.setImage(normalImage, for: .normal)
+        button.setImage(selectedImage, for: .selected)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        [imageView, titleLabel, dateTitleStackView, dateDetailStackView, rankLabel, starButton].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 3/4),
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            dateTitleStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            dateTitleStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dateTitleStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            dateDetailStackView.topAnchor.constraint(equalTo: dateTitleStackView.bottomAnchor),
+            dateDetailStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dateDetailStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            dateDetailStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            rankLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            rankLabel.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 1/4),
+            rankLabel.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -10),
+            
+            starButton.widthAnchor.constraint(equalToConstant: 50),
+            starButton.heightAnchor.constraint(equalToConstant: 50),
+            starButton.trailingAnchor.constraint(equalTo: trailingAnchor),
+            starButton.bottomAnchor.constraint(equalTo: rankLabel.bottomAnchor),
+        ])
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    func setupCell(entity: TopEntity) {
+        self.titleLabel.text = entity.title
+//        self.imageView.image
+        self.rankLabel.text = "# \(entity.rank)"
+        self.starButton.isSelected = entity.isFavorite ?? false
+        self.dateDetails[0].text = entity.startDate
+
+        if let endDate = entity.endDate {
+            self.dateDetails[1].text = endDate
+        } else {
+            self.dateDetails[1].text = ""
+        }
+    }
+}
+
+// MARK: - Top
+struct TopEntity {
+    let rank: Int
+    let title: String
+    let url: String
+    let imageURL: String
+    let type: TypeEnum
+    let startDate: String
+    let endDate: String?
+    let isFavorite: Bool?
+
+    enum TypeEnum: String {
+        case tv = "TV"
+        case movie = "Movie"
+        case ova = "OVA"
+        case special = "Special"
+        case music = "Music"
+        case ona = "ONA"
+        case manga = "Manga"
+        case novel = "Novel"
+        case oneShot = "One-shot"
+        case doujinshi = "Doujinshi"
+        case manhwa = "Manhwa"
+        case manhua = "Manhua"
+    }
+}
--- MyAnimeList/ViewController.swift
+++ MyAnimeList/ViewController.swift
@@ -11,6 +11,125 @@ class ViewController: UIViewController {
     
     private var viewModel: String
     
+    private let typeNames = ["Anime","Manga","Favorite"]
+    private let typeSubtype = ["anime":["airing","upcoming","tv","movie","ova","special","bypopularity","favorite"],"manga":["manga","novels","oneshots","doujin","manhwa","manhua","bypopularity","favorite"],"favorite":[]]
+    private var tmpData = ["airing","upcoming","tv","movie","ova","special","bypopularity","favorite"]
+    
+    
+    var horizontalScrollLayoutSection: NSCollectionLayoutSection {
+            let space: Double = 10
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(200), heightDimension: .absolute(318))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = space
+            section.contentInsets = NSDirectionalEdgeInsets(top: space, leading: space, bottom: space, trailing: space)
+            section.orthogonalScrollingBehavior = .continuous
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50))
+            let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+            section.boundarySupplementaryItems = [header]
+            return section
+    }
+        
+    var verticalScrollLayoutSection: NSCollectionLayoutSection {
+            let space: Double = 10
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/2))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = space
+            section.contentInsets = NSDirectionalEdgeInsets(top: space, leading: space, bottom: space, trailing: space)
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(10))
+            let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+            section.boundarySupplementaryItems = [header]
+            return section
+    }
+
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        //section的間距
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        //cell間距
+        layout.minimumLineSpacing = 5
+        //cell 長寬
+        layout.itemSize = CGSize(width: 100, height: 30)
+        //layout.estimatedItemSize
+        //滑動的方向
+        layout.scrollDirection = .horizontal
+        
+        
+        let space: Double = 10
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(200), heightDimension: .fractionalHeight(1))
+//        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 2)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = space
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: space, bottom: 0, trailing: space)
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.scrollDirection = .horizontal
+        let compositionalLayout =  UICollectionViewCompositionalLayout(section: section, configuration: configuration)
+        
+        let dynamicLayout = UICollectionViewCompositionalLayout { [unowned self] section, environment in
+            if section.isMultiple(of: 2) {
+                return self.horizontalScrollLayoutSection
+            } else {
+                return self.verticalScrollLayoutSection
+            }
+        }
+        
+        let view = UICollectionView(frame: .zero, collectionViewLayout: dynamicLayout)
+        view.register(ItemCell.self, forCellWithReuseIdentifier: "\(ItemCell.self)")
+        view.delegate = self
+        view.dataSource = self
+        view.isPagingEnabled = true
+        view.collectionViewLayout.invalidateLayout()
+        return view
+    }()
+    
+    private lazy var types: [TypeView] = {
+        //only use in struct
+//        return [TypeView](repeating: TypeView(title: "QWERASDF"), count: 3)
+        
+        let typeName = typeSubtype.keys
+        print("typeName=\(typeName)")
+        var views = [TypeView]()
+        for type in typeNames {
+            let view = TypeView(title: type)
+            views.append(view)
+        }
+        return views
+    }()
+    
+    private lazy var topStackView: UIStackView = {
+        let topStackView = UIStackView(arrangedSubviews: types)
+        topStackView.axis = .horizontal
+        topStackView.distribution = .fillEqually
+        topStackView.spacing = 10.0
+        return topStackView
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Button", for: .normal)
+        button.addTarget(self, action: #selector(handleButtonTapped), for: .touchUpInside)
+        button.setTitleColor(.darkGray, for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 5.0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    @objc private func handleButtonTapped() {
+        print("Button tapped")
+    }
+    
     init(viewModel: String) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -28,6 +147,79 @@ class ViewController: UIViewController {
     
     private func setupUI() {
         self.view.backgroundColor = UIColor(displayP3Red: 224.0/255.0, green: 216.0/255.0, blue: 200.0/255.0, alpha: 1)
+        
+        view.addSubview(topStackView, anchors: [.leadingSafeArea(20), .trailingSafeArea(-20),.topSafeArea(10),.height(50)])
+        
+        view.addSubview(collectionView, anchors: [.leadingSafeArea(20), .trailingSafeArea(-20),.topSafeArea(70),.bottomSafeArea(-50)])
+        
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Collection view at row \(collectionView.tag) selected index path \(indexPath)")
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        tmpData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(ItemCell.self)", for: indexPath) as? ItemCell {
+            //tmpData[indexPath.row]
+
+            cell.setupCell(entity: TopEntity(rank: 2, title: "Imaizumin Chi wa Douyara Gal no Tamariba ni Natteru Rashii", url: "https://myanimelist.net/manga/124483/Imaizumin_Chi_wa_Douyara_Gal_no_Tamariba_ni_Natteru_Rashii", imageURL: "https://cdn.myanimelist.net/images/manga/1/242797.jpg?s=71736414410e4600ca16d063fb9e67e1", type: .doujinshi, startDate: "Aug 2019", endDate: nil, isFavorite: true))
+            
+            cell.backgroundColor = .cyan
+            return cell
+        }
+        fatalError("Unable to dequeue subclassed cell")
+    }
+}
+
+//extension ViewController: UICollectionViewDelegateFlowLayout {
+////    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+////        CGSize(width: collectionView.frame.width, height: 60)
+////    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        CGSize(width: 100, height: 50)
+//    }
+//}
+
+
+
+class TypeView: UIView {
+    private lazy var typeBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemPink
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
+    private lazy var typeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .black
+        return label
+    }()
+    
+    required init(title: String) {
+        super.init(frame: .zero)
+        addSubview(typeBackgroundView, anchors: [.leading(0), .trailing(0),.top(0),.bottom(0)])
+        addSubview(typeLabel, anchors: [.centerX(0), .centerY(0)])
+        
+        self.typeLabel.text = title
+    }
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
