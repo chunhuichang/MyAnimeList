f39bc85dac88a032e78d0d19febfed58b93d5f05
Chun Hui Chang <jill20021@gmail.com>
2022-04-28 12:27:43 +0800

    test: implement local data test
    favorite/unfavorite
    go to  webview

--- MyAnimeListTests/MyAnimeListViewModelTests.swift
+++ MyAnimeListTests/MyAnimeListViewModelTests.swift
@@ -11,6 +11,8 @@ import Nimble
 
 class MyAnimeListViewModelTests: XCTestCase {
     
+    var gotoWebVCEntity: TopEntity?
+    
     private class MockUseCase: TopListUseCase {
         func fetchTop(queryString: String, with completion: @escaping (Result<[TopEntity], DataLoaderError>) -> Void) {
             guard let result = fetchDataTopResult else {
@@ -68,34 +70,54 @@ class MyAnimeListViewModelTests: XCTestCase {
         sut.input.fetchDataTrigger.value = ()
     }
     
-    // TODO: how to verify save data
-//    func test_TopListVM__whenSaveDataTrigger_thenLocalDataChange() {
-//        let predicateFetchEntity = [
-//            TopEntity(malID: 111, rank: 1, title: "title1", url: "url1", imageURL: "imageURL1", type: "Movie", startDate: "Apr 2017", isFavorite: true),
-//            TopEntity(malID: 222, rank: 3, title: "title2", url: "url2", imageURL: "imageURL2", type: "TV", startDate: "Apr 2015")]
-//        let predicateLocalEntity = [TopEntity(malID: 111, rank: 1, title: "title1", url: "url1", imageURL: "imageURL1", type: "Movie", startDate: "Apr 2017", isFavorite: true)]
-//
-//        var usecase = MockUseCase()
-//        usecase.fetchDataTopResult = .success(predicateFetchEntity)
-//        usecase.localTopData = predicateLocalEntity
-//
-//        let sut = makeSUT(usecase: usecase)
-//
-//        sut.listData.binding(trigger: false) { newValue, _ in
-//            if let entities = newValue {
-//                expect(entities.count) == predicateFetchEntity.count
-//                expect(entities[0].malID) == predicateFetchEntity[0].malID
-//            } else {
-//                XCTFail("resultData is nil")
-//            }
-//        }
-//
-//
-//        sut.input.saveDataTrigger.value = ()
-//    }
+    func test_TopListVM__whenFavoriteListTriggerUnfavorite_thenLocalDataDiscrease() {
+        let predicateLocalEntity = [
+            TopEntity(malID: 111, rank: 1, title: "title1", url: "url1", imageURL: "imageURL1", type: "Movie", startDate: "Apr 2017", isFavorite: true),
+            TopEntity(malID: 222, rank: 3, title: "title2", url: "url2", imageURL: "imageURL2", type: "TV", startDate: "Apr 2015", isFavorite: true)]
+        let unfavoriteEntity = TopEntity(malID: 111, rank: 1, title: "title1", url: "url1", imageURL: "imageURL1", type: "Movie", startDate: "Apr 2017", isFavorite: false)
+        
+        let usecase = MockUseCase(fetchDataTopResult: nil, localTopData: predicateLocalEntity)
+        let sut = makeSUT(usecase: usecase)
+        sut.input.typeClick(index: 2)
+        
+        sut.listData.binding(trigger: false) { newValue, _ in
+            if let entities = newValue {
+                expect(entities.count) == predicateLocalEntity.count - 1
+                expect(entities).toNot(contain(unfavoriteEntity))
+            } else {
+                XCTFail("resultData is nil")
+            }
+        }
+        
+        sut.input.saveDataTrigger.value = unfavoriteEntity
+    }
     
+    func test_TopListVM__whenFetchListTriggerFavorite_thenLocalDataIncrease() {
+        let predicateFetchEntity = [
+            TopEntity(malID: 111, rank: 1, title: "title1", url: "url1", imageURL: "imageURL1", type: "Movie", startDate: "Apr 2017", isFavorite: false),
+            TopEntity(malID: 222, rank: 3, title: "title2", url: "url2", imageURL: "imageURL2", type: "TV", startDate: "Apr 2015", isFavorite: false)]
+        let favoriteEntity = TopEntity(malID: 111, rank: 1, title: "title1", url: "url1", imageURL: "imageURL1", type: "Movie", startDate: "Apr 2017", isFavorite: true)
+        
+        let usecase = MockUseCase(fetchDataTopResult: .success(predicateFetchEntity), localTopData: nil)
+        let sut = makeSUT(usecase: usecase)
+        sut.input.typeClick(index: 0)
+        
+        sut.listData.binding(trigger: false) { newValue, _ in
+            if let entities = newValue {
+                expect(entities.count) == predicateFetchEntity.count
+                expect(entities).to(contain(favoriteEntity))
+                if let entity = entities.first(where: {$0.malID == favoriteEntity.malID}) {
+                    expect(entity.isFavorite) == true
+                }
+            } else {
+                XCTFail("resultData is nil")
+            }
+        }
+        
+        sut.input.saveDataTrigger.value = favoriteEntity
+    }
     
-    func test_TopListVM__whentypeClick_thenSubtypeChange() {
+    func test_TopListVM__whenTypeClick_thenSubtypeChange() {
         
         let predicateFetchEntity = [
             TopEntity(malID: 111, rank: 1, title: "title1", url: "url1", imageURL: "imageURL1", type: "Movie", startDate: "Apr 2017", isFavorite: true),
@@ -128,7 +150,32 @@ class MyAnimeListViewModelTests: XCTestCase {
         sut.input.typeClick(index: typeClickIndex)
     }
     
+    func test_TopListVM__whenListItemClick_thenGoToWebView() {
+        let usecase = MockUseCase(fetchDataTopResult: nil, localTopData: nil)
+        let sut = makeSUT(usecase: usecase)
+        sut.delegate = self
+        
+        let selectedEntity = TopEntity(malID: 111, rank: 1, title: "title1", url: "url1", imageURL: "imageURL1", type: "Movie", startDate: "Apr 2017", isFavorite: true)
+        sut.input.gotoWebVC(entity: selectedEntity)
+        
+        expect(self.gotoWebVCEntity).toEventually(equal(selectedEntity))
+    }
+    
     private func makeSUT(usecase: MockUseCase) -> TopListViewModel {
         return TopListViewModel(usecase)
+    }
+}
+
+extension TopEntity: Equatable {
+    public static func == (lhs: TopEntity, rhs: TopEntity) -> Bool {
+        lhs.malID == rhs.malID
+    }
+}
+
+
+extension MyAnimeListViewModelTests: AppCoordinatorDelegate {
+    func gotoWebVC(entity: TopEntity) {
+        print("jill gotoWebVC:\(entity.malID)")
+        self.gotoWebVCEntity = entity
     }
 }
